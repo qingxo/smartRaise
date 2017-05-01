96a1fac3c67b93dfc07c3a43086254eff5491ee9
taoqun <taoqun@317hu.com>
2017-05-01 22:20:24 +0800

    登录模块的md5.js模块的添加

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -5,18 +5,18 @@ import { fromPromise } from 'rxjs/observable/fromPromise';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import storage from '../shared/storage';
-// import * as md5 from 'md5';
+import * as md5 from 'md5';
 
 @Injectable()
 export class LoginService {
   constructor(private http:Http){}
   login(data:any):Observable<any> {
 
-    // var datas = {
-    //   "password":md5('123456'),
-    //   "userName":"sys"
-    // }
-    return this.http.post('api/web/login?','').map((res)=>{res.json()})
+    var datas = {
+      "password":md5('123456'),
+      "userName":"sys"
+    }
+    return this.http.post('api/web/login?'+storage.serialize(datas),'').map((res)=>{res.json()})
   }
 
   // isLogin() {
