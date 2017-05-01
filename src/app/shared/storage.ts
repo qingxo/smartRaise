caaf82df6d852daacca63cb79a5554bcf976c9e0
taoqun <taoqun@317hu.com>
2017-05-01 22:11:06 +0800

    新增了一出处理请求文件头的修改和异步请求的参数序列化问题

---
+++ src/app/shared/storage.ts
@@ -0,0 +1,61 @@
+class Storage {
+  constructor() {}
+  get(name) {
+    let value = localStorage.getItem(name)
+    if (!value) {
+      return null
+    }
+    try {
+      value = JSON.parse(value)
+    } catch (e) {
+      console.error(e)
+    }
+    return value
+  }
+
+  set(name, value) {
+    localStorage.setItem(name, JSON.stringify(value))
+  }
+
+  remove(name) {
+    localStorage.removeItem(name)
+  }
+
+  serialize(obj) {
+    var query = '',
+      name,
+      value,
+      fullSubName,
+      subName,
+      subValue,
+      innerObj,
+      i
+
+    for (name in obj) {
+      value = obj[name]
+
+      if (value instanceof Array) {
+        for (i = 0; i < value.length; ++i) {
+          subValue = value[i]
+          fullSubName = name + '[' + i + ']'
+          innerObj = {}
+          innerObj[fullSubName] = subValue
+          query += this.serialize(innerObj) + '&'
+        }
+      } else if (value instanceof Object) {
+        for (subName in value) {
+          subValue = value[subName]
+          fullSubName = name + '[' + subName + ']'
+          innerObj = {}
+          innerObj[fullSubName] = subValue
+          query += this.serialize(innerObj) + '&'
+        }
+      } else if (value !== undefined && value !== null)
+        {query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&'}
+    }
+
+    return query.length ? query.substr(0, query.length - 1) : query
+  }
+}
+
+export default new Storage()
