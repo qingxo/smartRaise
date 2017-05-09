3ca1d6b7ddda10b545385503d4782260337f6e05
taoqun <taoqun@317hu.com>
2017-05-09 15:05:33 +0800

    配置安排专业的路由等

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,16 +4,20 @@ import {HomeComponent} from './home.component';
 import { ClientComponent } from './client/client.component';
 import { ROUTER_CONFIG } from './home.routes';
 import {RouterModule} from '@angular/router';
-import {SharedModule} from '../shared'
+import {SharedModule} from '../shared';
+import { PlanmanComponent } from './planman/planman.component'
+import {FormsModule} from '@angular/forms'
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
+    FormsModule,
     RouterModule.forChild(ROUTER_CONFIG)
   ],
   declarations: [
     HomeComponent,
-    ClientComponent
+    ClientComponent,
+    PlanmanComponent
   ]
 })
 export class HomeModule { }
--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './home.component';
 import {ClientComponent} from './client';
+import {PlanmanComponent} from './planman';
 export const ROUTER_CONFIG: Routes = [
   {
     path:'',component:HomeComponent,
     children:[
       { path: '' , redirectTo:'/home/client', pathMatch:'full'},
-      { path: 'client' ,component:ClientComponent}
+      { path: 'client' ,component:ClientComponent},
+      { path: 'client/planman',component:PlanmanComponent}
     ]
   }
   ];
