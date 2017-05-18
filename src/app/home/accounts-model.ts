c710d5079640883801ecadcd21c832a91a24796c
taoqun <taoqun@317hu.com>
2017-05-18 22:17:13 +0800

    新增文件

---
+++ src/app/home/accounts-model.ts
@@ -0,0 +1,15 @@
+export class NormalAccount{
+  role:number = 3 //0 系统账号，1 平台账号，2 健康专员账号， 3 客户账号
+  name:string = null
+  cardId:string = null
+  mobile:number = null
+  headImg:string = null
+  healthPerson:string = null
+  sex:string = 'M'
+  birdthDay:string = null
+  height:number = null
+  weight:number = null
+  address:string = null
+  guardian:string = null
+
+}
