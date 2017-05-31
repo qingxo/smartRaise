ea1e75d2acf54d13a17b78179454d07740ba2b14
taoqun <taoqun@317hu.com>
2017-05-31 09:25:32 +0800

    生成省份中验证的指令

--- src/app/shared/moreDirective/card-check.directive.ts
+++ src/app/shared/moreDirective/card-check.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
-
+import { Directive ,OnChanges,SimpleChanges,Input} from '@angular/core';
+import {Validator,Validators,ValidatorFn,NG_VALIDATORS,AbstractControl} from '@angular/forms'
 @Directive({
   selector: '[appCardCheck]'
 })
-export class CardCheckDirective {
+export class CardCheckDirective implements Validator,OnChanges {
+  @Input() appCardCheck:string
+  private valFn = Validators.nullValidator;
 
-  constructor() { }
+  ngOnChanges(changes:SimpleChanges):void {
+
+  }
+
+  validate(control:AbstractControl):{[key:string]:any}{
+    return this.valFn(control)
+  }
 
 }
