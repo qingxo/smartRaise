3de013505f7ceb2da622dd2a180580e21bcf87d4
taoqun <taoqun@317hu.com>
2017-06-14 23:16:48 +0800

    1、新版框架的登录样式修改完成 2、一些图片的添加

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,36 +8,63 @@ import storage from '../shared/storage';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  providers:[LoginService]
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
 
-  private loginAction:Observable<any>
-  private passWord:string = ''
-  private loginName:string ='sys'
-  private errorMsg:string = ''
-  constructor(private loginService:LoginService,private router:Router) {
+  private loginAction: Observable<any>
+  private passWord: string = ''
+  private loginName: string = 'sys'
+  private errorMsg: string = ''
+  private loginErr: boolean
+  private role: number
+  private rememberPWD: boolean
+  constructor(private loginService: LoginService, private router: Router) {
   }
 
   ngOnInit() {
   }
 
-  login() {
-    var datas = {
-      "password":md5(this.passWord),
-      "userName":this.loginName
+  loginHandler() {
+    console.log("click")
+    if (!this.passWord && !this.loginName) {
+      this.loginErr = true
+      this.errorMsg = '请输入用户名和密码'
+      return
     }
-    this.loginService.login(datas).subscribe(
-      (data) =>{
-        storage.set('state',data.data)
-        if(data.success){
+
+    if (!this.loginName) {
+      this.loginErr = true
+      this.errorMsg = '请输入用户名'
+      return
+    }
+    if (!this.passWord) {
+      this.loginErr = true
+      this.errorMsg = '请输入密码'
+      return
+    }
+
+    if (this.rememberPWD) {
+      let data = { 'userName': this.loginName, 'password': window.btoa(this.passWord) }
+      storage.set('ywLogin', data)
+    } else {
+      storage.remove('ywLogin')
+    }
+
+    var loginParam = 'userName=' + this.loginName + '&password=' + md5(this.passWord)
+
+    this.loginService.login(loginParam)
+      .subscribe(
+      (data) => {
+        storage.set('state', data.data)
+        if (data.success) {
           this.router.navigate(['/home/client'])
-        }else{
-        this.errorMsg = data.errMsg
+        } else {
+          this.errorMsg = data.errMsg
+          this.loginErr = true
         }
       }
-    )
+      )
+
   }
-
-
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http,Response} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import { fromPromise } from 'rxjs/observable/fromPromise';
 import 'rxjs/add/operator/catch';
@@ -8,9 +8,9 @@ import storage from '../shared/storage';
 
 @Injectable()
 export class LoginService {
-  constructor(private http:Http){}
-  login(data:any):Observable<any> {
-    return this.http.post('api/web/login?'+storage.serialize(data),'').map((res)=>{return res.json() })
+  constructor(private http: Http) { }
+  login(data: any): Observable<any> {
+    return this.http.post('api/web/login?' + data, '').map((res) => { return res.json() })
   }
 
   // isLogin() {
