9cc7722ae4e4f1da874203a3d3d99ec7c1978937
taoqun <taoqun@317hu.com>
2017-06-16 12:27:47 +0800

    全局的样式替换

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,24 +8,25 @@ import * as $ from 'jquery'
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  private toggleLogOutFlag:boolean = true;
-  private userName:any = '';
-  private icon:any = '';
+  private toggleLogOutFlag: boolean = true;
+  private userName: any = '';
+  private userNameState: string
+  private icon: any = '';
 
-  constructor(private router:Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.initUserInfo()
   }
 
 
-    logOut() {
-      storage.remove('state');
-      this.router.navigate(['/login'])
-    }
+  logOut() {
+    storage.remove('state');
+    this.router.navigate(['/login'])
+  }
 
   initUserInfo() {
-    this.userName = storage.get('state')['userName'];
+    this.userNameState = storage.get('state')['userName'];
     this.icon = storage.get('state')['icon'];
 
   }
