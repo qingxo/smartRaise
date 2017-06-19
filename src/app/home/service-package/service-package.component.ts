999df35bcfe3adaa97be3b5bbc5bd3e13f8af7c6
taoqun <taoqun@317hu.com>
2017-06-19 09:20:54 +0800

    服务包管理页面的样式调整

--- src/app/home/service-package/service-package.component.ts
+++ src/app/home/service-package/service-package.component.ts
@@ -5,23 +5,23 @@ import {SweetAlertService} from 'ng2-sweetalert2'
   selector: 'app-service-package',
   templateUrl: './service-package.component.html',
   styleUrls: ['./service-package.component.scss'],
-  providers:[ServicePackageService,SweetAlertService]
+  providers: [ServicePackageService, SweetAlertService]
 })
 export class ServicePackageComponent implements OnInit {
-  private pageSize:number = 10
-  private pageNumber:number =1
-  private pages:Array<any> = []
-  private list:Array<any> = []
-  private queryInfo:string = ''
-
-  constructor(private servicePackageService:ServicePackageService,private sweetAlertService:SweetAlertService) { }
+  private pageSize: number = 10
+  private pageNumber: number = 1
+  private pages: Array<any> = []
+  private list: Array<any> = []
+  private queryInfo: string = ''
+  private totalPage: string
+  constructor(private servicePackageService: ServicePackageService, private sweetAlertService: SweetAlertService) { }
 
   ngOnInit() {
     this.packageList()
   }
 
   handlePackageConfirm(item) {
-    let msg = item.statue =='0'?'上架':'下架'
+    let msg = item.statue == '0' ? '上架' : '下架'
     this.sweetAlertService.swal({
       title: `确认需要${msg}?`,
       type: 'warning',
@@ -29,31 +29,31 @@ export class ServicePackageComponent implements OnInit {
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: '确定',
-      cancelButtonText:'取消'
+      cancelButtonText: '取消'
     }).then(() => {
       this.handlePackageStatus(item)
-        },(dismiss)=>{
-      })
-  }
-
-  handlePackageStatus(data) {
-    let status = typeof data.item.statue === 'undefined'?0:parseInt(data.item.statue)
-    status === 1? status = 0 : status=1
-    let param ={'servicePackId':data.item.servicePackId,'statue':status}
-
-    this.servicePackageService.changeStatus(param).subscribe((res)=>{
-        if (res.success) {
-            this.sweetAlertService.swal(status===0?'下架成功':'上架成功',"",'success')
-            status==1?data.item.statue=1:data.item.statue=0
-        }else{
-          this.sweetAlertService.swal(res.errMsg,'','error')
-        }
+    }, (dismiss) => {
     })
   }
 
-  searchTable(queryInfo:string) {
+  handlePackageStatus(data) {
+    let status = typeof data.item.statue === 'undefined' ? 0 : parseInt(data.item.statue)
+    status === 1 ? status = 0 : status = 1
+    let param = { 'servicePackId': data.item.servicePackId, 'statue': status }
+
+    this.servicePackageService.changeStatus(param).subscribe((res) => {
+      if (res.success) {
+        this.sweetAlertService.swal(status === 0 ? '下架成功' : '上架成功', "", 'success')
+        status == 1 ? data.item.statue = 1 : data.item.statue = 0
+      } else {
+        this.sweetAlertService.swal(res.errMsg, '', 'error')
+      }
+    })
+  }
+
+  searchTable(queryInfo: string) {
     this.queryInfo = queryInfo
-    this.pageNumber =1
+    this.pageNumber = 1
     this.packageList()
   }
 
@@ -64,16 +64,16 @@ export class ServicePackageComponent implements OnInit {
 
   packageList() {
     let data = {
-      "pageSize":this.pageSize,
-      "pageNum":this.pageNumber,
-      "query":this.queryInfo
+      "pageSize": this.pageSize,
+      "pageNum": this.pageNumber,
+      "query": this.queryInfo
     }
     this.servicePackageService.packageList(data).subscribe((res) => {
       if (res.success) {
         this.list = res.data.result
         this.pages = res.data.linkPageNumbers
         this.pageNumber = res.data.pageNumber
-
+        this.totalPage = res.data.totalCount
       }
 
     })
