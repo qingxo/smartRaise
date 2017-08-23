3cf3763e63f46d7c5eaef77a6ac78de09d5baed6
taoqun <taoqun@317hu.com>
2017-08-23 16:39:14 +0800

    1、server去除qiniu支持 2、修改了proxy的接口，原来的接口以及要在新版中废除了~

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const proxy = require('express-http-proxy');
 const helmet = require('helmet');
 const compression = require('compression')
 const config = require('./config');
-const qiniu = require('qiniu')
-const bucket = config.bucket
+// const qiniu = require('qiniu')
+// const bucket = config.bucket
 
 //需要填写你的 Access Key 和 Secret Key
-qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
-qiniu.conf.SECRET_KEY = config.SECRET_KEY;
+// qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
+// qiniu.conf.SECRET_KEY = config.SECRET_KEY;
 
 // express 实例
 const app = express();
@@ -29,28 +29,23 @@ app.use(compression());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 //构建上传策略函数
-app.get('/uploadToken',function(req,res) {
- // return  function uptoken(bucket, key) {
-    var putPolicy = new qiniu.rs.PutPolicy(bucket);
- //    return putPolicy.token();
- //  }
- res.send({"uptoken":putPolicy.token()})
-})
-
-
+// app.get('/uploadToken', function(req, res) {
+//   var putPolicy = new qiniu.rs.PutPolicy(bucket);
+//   res.send({"uptoken": putPolicy.token()})
+// })
 
 // api proxy
 app.use('/api', proxy(config.api, {
-  forwardPath: function (req, res) {
-    return require('url').parse("/pc"+req.url).path;
+  proxyReqPathResolver: function(req, res) {
+    return require('url').parse("/pc" + req.url).path;
   }
 }));
 
-app.get('*', function (req, res) {
+app.get('*', function(req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 });
 
 const port = config.port || process.env.PORT
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('🌎 => App is running on port %s', port)
 })
