0d7b1df9d8f0bbb6354973f9c809df0411e279f2
taoqun <taoqun@317hu.com>
2017-08-23 15:08:38 +0800

    1、分页组件进行了修改

--- src/app/shared/pages/pages.component.ts
+++ src/app/shared/pages/pages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
@@ -9,11 +9,31 @@ export class PagesComponent implements OnInit {
 
   @Input() pages: Array<any> = []
   @Input() currentPage: number = 1
-  @Input() sum: number = 1
+  @Input() sum: number = 0
+  @Input() pageSize: number = 10
   @Output() pageInfo = new EventEmitter<any>()
+  @ViewChild('hh') hh: ElementRef
+  @ViewChild('tt') tt: ElementRef
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewChecked() {
+    if (this.hh && this.tt) {
+      let t = this.getLastPage()
+      if (t > 12) {
+        if (this.tt && this.hh) {
+          this.hh.nativeElement.className = 'p-head'
+          this.tt.nativeElement.className = 'p-tail'
+        }
+      }
+    }
+  }
+
+  getLastPage() {
+    return Math.ceil(this.sum / this.pageSize)
   }
 
   changePage(num) {
@@ -21,7 +41,6 @@ export class PagesComponent implements OnInit {
     if (pageNum < 1) {
       pageNum = 1
     }
-
     if (pageNum > this.pages[this.pages.length - 1]) {
       pageNum = this.pages[this.pages.length - 1]
     }
@@ -29,7 +48,10 @@ export class PagesComponent implements OnInit {
     if (pageNum != this.currentPage) {
       this.pageInfo.emit(pageNum)
     }
+  }
 
+  goLastPage() {
+    this.pageInfo.emit(this.getLastPage())
   }
 
 }
