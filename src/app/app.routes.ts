e085d92c581431b9888f20e2396858c95ea19285
taoqun <taoqun@317hu.com>
2017-09-20 14:06:17 +0800

    添加一个路由没有的情况下的默认地址

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import {LoginGuard} from './guard/LoginGuard';
-import {LoginComponent} from './login/login.component';
-import {DialogsComponent} from './dialogs';
+import { LoginGuard } from './guard/LoginGuard';
+import { LoginComponent } from './login/login.component';
+import { DialogsComponent } from './dialogs';
 
 export const ROUTER_CONFIG: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full', canActivate: [LoginGuard] },
   { path: 'home', loadChildren: 'app/home/home.module#HomeModule', canActivate: [LoginGuard] },
-  { path: 'popout', component: DialogsComponent, outlet: 'boom' }
+  { path: 'popout', component: DialogsComponent, outlet: 'boom' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full', canActivate: [LoginGuard] }
+
 ];
