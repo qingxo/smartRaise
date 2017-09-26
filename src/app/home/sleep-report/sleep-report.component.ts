a41aa7197229a61861af0733d337eb9cc3e29f21
taoqun <taoqun@317hu.com>
2017-09-26 17:17:05 +0800

    添加了睡眠报告的接口，需要调试，未完成

--- src/app/home/sleep-report/sleep-report.component.ts
+++ src/app/home/sleep-report/sleep-report.component.ts
@@ -1,29 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { HealthMonitorService } from '../health-monitor/health-monitor.service';
+import { SleepReportService } from './sleep-report.service';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-sleep-report',
   templateUrl: './sleep-report.component.html',
   styleUrls: ['./sleep-report.component.scss'],
-  providers: [HealthMonitorService]
+  providers: [HealthMonitorService, SleepReportService]
 })
 export class SleepReportComponent implements OnInit {
 
   private userInfo: any = {};
   private customerId: string = '';
   private equipNo: string = '';
-  constructor(private healthMonitorService: HealthMonitorService, private route: ActivatedRoute) { }
+  private maxDay: string = moment(new Date()).format('YYYY-MM-DD');
+  private reportDay: string = moment(new Date()).format('YYYY-MM-DD');
+  private list: any = {};
+  constructor(private healthMonitorService: HealthMonitorService, private sleepReportService: SleepReportService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.customerId = this.route.snapshot.params['customerId'];
     this.equipNo = this.route.snapshot.params['equipNo'];
-
-    this.getUserInfo()
+    this.getUserInfo();
+    this.callService();
   }
 
   changeDays(val) {
-    console.log(val)
+    if (val !== this.reportDay) {
+      this.reportDay = val;
+      this.callService()
+    }
+
+  }
+
+  callService() {
+    this.sleepReportService.reportDetail(this.equipNo, this.reportDay).subscribe((res) => {
+      if (res.code === 200) {
+        this.list = res;
+      }
+
+    })
   }
 
   getUserInfo() {
--- src/app/home/sleep-report/sleep-report.service.ts
+++ src/app/home/sleep-report/sleep-report.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { BaseService } from '../../shared/base.service';
 
 @Injectable()
-export class SleepReportService {
+export class SleepReportService extends BaseService {
 
-  constructor() { }
+  constructor(http: Http) { super(http) }
+
+  reportDetail(equipmentNo: string, date: string) {
+    return this.getInfo(`api/webHealthManage/getSleepReport/${equipmentNo}/${date}`);
+  }
 
 }
