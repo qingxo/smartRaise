d038c9f3019cdb0d01a3dc3fd4a9d30fa70afde1
taoqun <taoqun@317hu.com>
2017-09-28 16:39:57 +0800

    完善了院内档案的接口等信息，未完成

--- src/app/home/record-detail/record-detail.component.ts
+++ src/app/home/record-detail/record-detail.component.ts
@@ -1,15 +1,111 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { RecordDetailService } from './record-detail.service';
 @Component({
   selector: 'app-record-detail',
   templateUrl: './record-detail.component.html',
-  styleUrls: ['./record-detail.component.scss']
+  styleUrls: ['./record-detail.component.scss'],
+  providers: [RecordDetailService]
 })
 export class RecordDetailComponent implements OnInit {
 
-  constructor() { }
+  public customerBaseInfo: any = {};
+  public customerId: string = '';
+  public seeDoctor: any = {};
+  public opInfo: Array<any> = [];
+  public outpaInfo: Array<any> = [];
+  public hosInfo: any = [];
+  public surveyInfo: any = [];
+  public inspectInfo: any = [];
+  public docOrderInfo: any = [];
+  public dateChoose: Array<string> = ['全部', '2014-9-1', '2015-9-1', '2017-09-9'];
+  public docDate: any = '全部';
+  public sendDate: string = '';
+  constructor(public recordDetailService: RecordDetailService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // this.customerId = this.route.snapshot.params['customerId'];
+    this.customerId = '1705101137488803263'
+    this.initData();
+  }
+
+  onChange(val) {
+    if (this.docDate == '全部') {
+      this.sendDate = '';
+    } else {
+      this.sendDate = this.docDate;
+    }
+    this.initDoctorOrderInfo()
+  }
+
+  initData() {
+    this.recordDetailService.baseInfo(this.customerId).subscribe((res) => {
+      this.customerBaseInfo = res;
+
+    })
+    this.initSeeDoctorInfo();
+    this.initOperatorInfo();
+    this.initOutPatientInfo();
+    this.initHispitalInfo();
+    this.initSurveyReportInfo();
+    this.initInspectInfo();
+    this.initDoctorOrderInfo();
+  }
+
+
+  // 医嘱信息
+  initDoctorOrderInfo() {
+    this.recordDetailService.doctorAdviceInfo(this.customerId, this.sendDate).subscribe((res) => {
+      console.log(res)
+      this.docOrderInfo = res;
+    })
+  }
+
+  // 检查报告
+  initInspectInfo() {
+    this.recordDetailService.inspectionReportInfo(this.customerId).subscribe((res) => {
+      this.inspectInfo = res;
+    })
+  }
+
+
+  // 检验报告
+  initSurveyReportInfo() {
+    this.recordDetailService.surveyReportInfo(this.customerId).subscribe((res) => {
+      this.surveyInfo = res;
+    })
+  }
+
+  // 住院信息
+  initHispitalInfo() {
+    this.recordDetailService.hospitalInfo(this.customerId).subscribe((res) => {
+      this.hosInfo = res;
+    })
+  }
+
+  // 就诊信息
+  initSeeDoctorInfo() {
+    this.recordDetailService.seeDoctorInfo(this.customerId).subscribe((res) => {
+      this.seeDoctor = res;
+    })
+  }
+
+  /**
+    住院手术
+   */
+  initOperatorInfo() {
+    this.recordDetailService.operatorInfo(this.customerId).subscribe((res) => {
+      this.opInfo = res;
+    })
+  }
+
+  /**
+    门诊信息
+   */
+  initOutPatientInfo() {
+    this.recordDetailService.outPatientInfo(this.customerId).subscribe((res) => {
+      this.outpaInfo = res;
+    })
   }
 
 }
--- src/app/home/record-detail/record-detail.service.ts
+++ src/app/home/record-detail/record-detail.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
-
+import storage from '../../shared/storage';
+import { Http } from '@angular/http';
+import { BaseService } from '../../shared/base.service';
 @Injectable()
-export class RecordDetailService {
+export class RecordDetailService extends BaseService {
 
-  constructor() { }
+  constructor(public http: Http) { super(http); }
 
+  baseInfo(customerId) {
+    return this.postInfo(`api/customer/hospitalRecord/basicInformation/${customerId}`, '');
+  }
+
+  seeDoctorInfo(customerId) {
+    return this.postInfo(`api/customer/hospitalRecord/medicalInformation/${customerId}`, '');
+  }
+
+  operatorInfo(customerId) {
+    return this.postInfo(`api/customer/hospitalRecord/receivingOperations/${customerId}`, '');
+  }
+
+  hospitalInfo(customerId) {
+    return this.postInfo(`api/customer/hospitalRecord/inpatientsInformation/${customerId}`, '');
+  }
+
+  surveyReportInfo(customerId) {
+    return this.postInfo(`api/customer/hospitalRecord/surveyReport/${customerId}`, '');
+  }
+
+  doctorAdviceInfo(customerId, date) {
+    return this.postInfo(`api/customer/hospitalRecord/doctorOrders/${customerId}/${date}`, '');
+  }
+
+  outPatientInfo(customerId) {
+    return this.postInfo(`api/customer/hospitalRecord/outpatientPrescription/${customerId}`, '');
+  }
+
+  inspectionReportInfo(customerId) {
+    return this.postInfo(`api/customer/hospitalRecord/inspectionReport/${customerId}`, '');
+  }
 }
