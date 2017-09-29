22a8517e7775e991507934e30d0ed19e02fc73ed
taoqun <taoqun@317hu.com>
2017-09-29 11:21:32 +0800

    添加鞋子接口

--- src/app/home/shoes-map/shoes-map.service.ts
+++ src/app/home/shoes-map/shoes-map.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import storage from '../../shared/storage';
+import { BaseService } from '../../shared/base.service';
 
 @Injectable()
-export class ShoesMapService {
+export class ShoesMapService extends BaseService {
 
-  constructor() { }
+  constructor(public http: Http) { super(http); }
+
+  shoesList(data) {
+    return this.postInfo('api/customer/listShoesByPage ', '');
+  }
+
+  getLocalPos(data) {
+    return this.postInfo('api/customer/getLoacal ', storage.serialize(data));
+  }
+
+  getAccidentInfo(data) {
+    return this.postInfo('api/customer/getAccident ', storage.serialize(data));
+
+  }
 
 }
